186841f537eeb01d1b185fdeae9f7c7db742c07d
Jian-Wuyou <dramante@up.edu.ph>
2023-05-22 12:36:57 +0800

    refactor: reorganized folder structure

--- heatwatch/app.py
+++ heatwatch/app.py
@@ -1,12 +1,9 @@
-from os import path
-
 from flask import Flask
 
 from . import init_routes
 
 # Flask app
-root_path = path.abspath(path.join(path.dirname(__file__), ".."))
-app = Flask(__name__, root_path=root_path)
+app = Flask(__name__)
 
 init_routes(app)
 
