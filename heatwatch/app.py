6e9004b1c84318ade40947ca82d6cccfa11fbaec
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2023-05-23 15:33:42 +0800

    added database queries

--- heatwatch/app.py
+++ heatwatch/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from .extensions import mysql
 
 from . import init_routes
 
 # Flask app
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'cs145_heathelm'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 init_routes(app)
 
--- heatwatch/controllers.py
+++ heatwatch/controllers.py
@@ -1,14 +1,36 @@
 from time import time
 from random import randint
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+import pymysql
+from .extensions import mysql
 
 controllers = Blueprint("controllers", __name__)
 
 @controllers.post("/update_db")
 def post_readings():
     """Receives data from the Arduino"""
+
+    #req = {"id":int,"time":int,"temperature":int,"humidity":int,"heatIndex":int}
+    req = request.json
     print(request.get_data(as_text=True))
-    return '{"success": true}', 200
+
+    ## query DB
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "INSERT INTO readings (id, `time`, temperature, humidity, `heat index`) VALUES (%s,%s,%s,%s,%s)"
+        args=[req["id"],req["time"],req["T"],req["RH"],req["HI"]]
+        cursor.execute(sql,args)  
+        conn.commit()
+        return '{"success": true}', 200
+
+    except Exception as e:
+        print(e)
+        return '{"success": false}', 500
+
+    finally:    
+        cursor.close() 
+        conn.close()
 
 @controllers.post("/readings")
 def get_readings():
@@ -22,9 +44,34 @@ def get_readings():
     if "to" not in data:
         data["to"] = round(time() * 1000)
 
-    start, end = data["from"], data["to"]
+    id, start, end = data["id"], data["from"], data["to"]
 
-    data = []
+    ## query DB
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql="SELECT * FROM readings WHERE id = %s AND `time` > %s AND `time` <= %s ORDER BY `time`"
+        args=[id, start, end]
+        cursor.execute(sql,args)
+        rows = cursor.fetchall() # result data
+
+        first, last = 0,0
+        if rows: # if not empty
+            first,last = rows[0]["time"], rows[-1]["time"] # get time of first and last entry
+        res = jsonify({'last':last, 'first':first, 'data': rows}) # return {last, first, data}
+ 
+        return res, 200
+    
+    except Exception as e:
+        print(e)
+        return '{"success":false}', 500
+
+    finally:
+        cursor.close() 
+        conn.close()
+
+    # generate fake data in a separate script
+    """
     # Generate fake data for testing
     for t in range(start+1, end+1, 200):
         data.append([t, randint(10, 100), 0, 0])
@@ -34,3 +81,4 @@ def get_readings():
         "first" : start+1,
         "last" : t
     }
+    """
