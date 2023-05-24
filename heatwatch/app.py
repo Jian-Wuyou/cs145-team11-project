302285d3f70c1172deda1e9fe9bb853e842f2c44
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2023-05-24 22:47:20 +0800

    sample database

--- heatwatch/app.py
+++ heatwatch/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from .extensions import mysql
 
 from . import init_routes
+from . import config  
 
 # Flask app
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'cs145_heathelm'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = config.MYSQL_DATABASE_USER
+app.config['MYSQL_DATABASE_PASSWORD'] = config.MYSQL_DATABASE_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = config.MYSQL_DATABASE_DB
+app.config['MYSQL_DATABASE_HOST'] = config.MYSQL_DATABASE_HOST
 
 mysql.init_app(app)
 
---
+++ heatwatch/config.py
@@ -0,0 +1,6 @@
+# for local testing; do not upload actual credentials 
+
+MYSQL_DATABASE_USER = 'root'
+MYSQL_DATABASE_PASSWORD = 'password'
+MYSQL_DATABASE_DB = 'cs145_heathelm'
+MYSQL_DATABASE_HOST = 'localhost'
