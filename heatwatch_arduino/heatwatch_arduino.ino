f77c8df98c51b3ef925b557e1f9eea3be66cef64
Tim4649 <91589185+Tim4649@users.noreply.github.com>
2023-05-28 12:53:45 +0800

    Updated heatwatch_arduino.ino
    Added the code for the Arduino sensor.

--- heatwatch_arduino/heatwatch_arduino.ino
+++ heatwatch_arduino/heatwatch_arduino.ino
@@ -1,7 +1,36 @@
-void setup() {
+// Library used: DHT sensor library by Adafruit (Arduino will ask to install another library)
 
+#include "DHT.h"
+
+#define DHTPIN 2                                               // Change Pin Number as needed
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
 }
 
 void loop() {
-    
+   delay(1000);                                                // Wait one second for the sensor (DHT 11)
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if(isnan(h) || isnan(t)){
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  float hi = dht.computeHeatIndex(t, h, false);                // Heat Index in degree celsius
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F(" Heat index: "));
+  Serial.print(hi);
+  Serial.print(F("°C\n"));
 }
