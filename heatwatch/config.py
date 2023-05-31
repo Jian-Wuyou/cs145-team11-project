e0556778847110ef3a9f08c8066048cba9920a58
Jian-Wuyou <dramante@up.edu.ph>
2023-05-31 14:52:39 +0800

    chore: remove unused files

--- heatwatch/config.py
+++
@@ -1,8 +0,0 @@
-# for local testing; do not upload actual credentials 
-import os
-
-MYSQL_DATABASE_USER = os.getenv("MYSQLUSER")
-MYSQL_DATABASE_PASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQL_DATABASE_DB = os.getenv("MYSQLDATABASE")
-MYSQL_DATABASE_HOST = os.getenv("MYSQLPORT")
-MYSQL_DATABASE_PORT = os.getenv("MYSQLPORT")
